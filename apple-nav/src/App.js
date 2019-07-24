50b3951f762e1736511d5180fdae304fceea632f
gisellehargrove <gisellehargrove@gmail.com>
2019-07-23 22:02:49 -0400

    creating SubNav component

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Route exact path="/" />
 
       {Data.map((routeLink, index) =>
-        <Route key={index} path={`/${routeLink}`}/>
+        <Route key={index} path={`/${routeLink}`} component={SubNav} />
       )}
     </div>
   );
---
+++ apple-nav/src/components/SubNav/SubNav.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SubNav = (props) => {
+
+};
+
+export default SubNav;
